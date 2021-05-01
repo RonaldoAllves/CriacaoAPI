608dd1caf1e92558fa9a05b004b95d6981c1917e
Ronaldo Allves <ronaldoalvesdebritopereira@Mac-mini-de-Ronaldo.local>
2021-05-01 11:28:10 -0300

    Já criado todas as funcoes da API exceto autenticacao

---
+++ CriacaoAPI/Controllers/PessoasController.cs
@@ -0,0 +1,97 @@
+﻿using CriacaoAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CriacaoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoasController : ControllerBase
+    {
+        private static List<Pessoa> pessoas = new List<Pessoa>();
+
+        //[HttpGet]
+        public List<Pessoa> Get()
+        {
+            return pessoas;
+        }
+
+        [HttpGet]
+        [Route("ID")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Pessoa> Get([FromQuery] int id)
+        {
+            var searchID = pessoas.Where(item => item.Id == id).FirstOrDefault();
+            //Pessoa p = pessoas.First(x => x.Id.Equals(id));
+            if (searchID != null){
+                return searchID;
+            }
+            
+            return NotFound();
+        }
+
+        [HttpGet]
+        [Route("UF")] 
+        public List<Pessoa> Get([FromQuery] string uf)
+        {
+            List<Pessoa> pessoasUF = new List<Pessoa>();
+
+            foreach (Pessoa p in pessoas)
+            {
+                if (p.Uf.Equals(uf)){
+                    pessoasUF.Add(p);
+                }
+            }
+
+            return pessoasUF;
+        }
+
+        [HttpPost]
+        [Route("Inserir")]
+        public async Task<ActionResult<Pessoa>> Post(
+            [FromBody]Pessoa model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                pessoas.Add(model);
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpDelete]
+        [Route("DeleteById")]
+        public void Delete(int id)
+        {
+            pessoas.RemoveAt(pessoas.IndexOf(pessoas.First(x => x.Id.Equals(id))));
+        }
+
+        [HttpPut]
+        [Route("Atualizar")]
+        public async Task<ActionResult<Pessoa>> Put(
+            [FromBody] Pessoa model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                int index = pessoas.IndexOf(pessoas.First(x => x.Id.Equals(model.Id)));
+                pessoas[index] = model;
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
---
+++ CriacaoAPI/Models/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CriacaoAPI.Models
+{
+    public class Pessoa
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo nome é obrigatorio")]
+        public String Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo UF é obrigatorio")]
+        public String Uf { get; set; }
+
+        [Required(ErrorMessage = "Campo Data é obrigatorio")]
+        public String Data { get; set; }
+
+        [Required(ErrorMessage = "Campo CPF é obrigatorio")]
+        public Int64 Cpf { get; set; }
+
+    }
+}
